6e20695a97b596ce34f74e2fc3a0af5be2064322
Vincent Germain <vgermain31@gmail.com>
2014-04-10 23:44:28 +0200

    ajout de la fonction screen

---
+++ autopilot/Autopilot/hw/screen/include/screen.hpp
@@ -0,0 +1,80 @@
+/*
+ * screen.hpp
+ *
+ *  Created on: 5 mars 2014
+ *      Author: vincent
+ */
+
+#ifndef SCREEN_HPP_
+#define SCREEN_HPP_
+
+#include <stdint.h>
+#include <hw/led/include/Led.hpp>
+#include <hw/pwm/include/Pwm.hpp>
+
+namespace hw {
+#define SLOW 64
+#define FAST 16
+#define VERY_SLOW 128
+#define BUZZ_CHANNEL 5 // RC_CHANNEL -1 (6 dans ce cas)
+
+typedef enum EScreenState{
+	S_ALL_ON=0,
+	S_ALL_OFF,
+	S_BLUE,
+	S_YELLOW,
+	S_RED,
+	S_BLUE_BS,
+	S_YELLOW_BS,
+	S_RED_BS,
+	S_BLUE_BF,
+	S_YELLOW_BF,
+	S_RED_BF,
+	S_WARNING_1,
+	S_WARNING_2,
+	S_WARNING_3,
+	S_WARNING_4,
+	S_OK_1,
+	S_OK_2,
+	S_OK_3,
+	S_CHENILLE_LS,//left slow
+	S_CHENILLE_LF,
+	S_CHENILLE_RS,
+	S_CHENILLE_RF,//right fast
+	S_BUZZ
+}ScreenState;
+
+class Screen {
+public:
+	Screen(hw::Pwm::Input& out,	hw::Pwm& pwm);
+	virtual ~Screen();
+
+	/* Display predifined state */
+	void Display(ScreenState S);
+
+protected:
+	ScreenState _state;
+	uint16_t ncycle,ncycle1,slow,fast;
+	hw::Led myBlueLed;
+	hw::Led myYellowLed;
+	hw::Led myRedLed;
+	hw::Pwm::Input& _out;
+	hw::Pwm& _pwm;
+private:
+	/* @brief switch on the led w/o state update */
+	//void _switchOn(void);
+
+	/* @brief switch off the led w/o state update */
+	//void _switchOff(void);
+public:
+	/* called by cyclic task for blink */
+	void update();
+};
+
+
+} /* namespace hw */
+
+
+
+
+#endif /* SCREEN_HPP_ */
--- autopilot/Autopilot/system/tasks/source/ControlCyclicTask.cpp
+++ autopilot/Autopilot/system/tasks/source/ControlCyclicTask.cpp
@@ -7,6 +7,7 @@
 
 #include <hw/serial/include/FastSerial.hpp>
 #include <system/system/include/System.hpp>
+#include <hw/Screen/include/Screen.hpp>
 
 #include "../include/ControlCyclicTask.hpp"
 
@@ -18,7 +19,7 @@ ControlCyclicTask::ControlCyclicTask(
         unsigned portBASE_TYPE uxPriority,
         uint16_t period,
         uint16_t delay) :
-	infra::CyclicTask(pcName,usStackDepth,uxPriority,period,delay)
+	infra::CyclicTask(pcName,usStackDepth,uxPriority,period,delay),myScreen(system::System::system.board.pwmVal,system::System::system.board.pwm)
 {
 }
 
@@ -33,6 +34,9 @@ void ControlCyclicTask::init(void)
 	system::System::system.initialize();
 
 	Task::init();
+	nbcycle = 0;//screen demo
+	nbcycle1 = 0;
+	myScreen.Display(hw::S_ALL_OFF);
 }
 
 void ControlCyclicTask::runCycle(void)
@@ -45,7 +49,48 @@ void ControlCyclicTask::runCycle(void)
 
 	system::System::system.ancs.smGlobal.execute();
 	system::System::system.getMavHouseKeeping().update();
-}
+	nbcycle = nbcycle +1;
+	switch (nbcycle1){
+	case 0:
+		myScreen.Display(hw::S_ALL_ON);
+		break;
+	case 1:
+		myScreen.Display(hw::S_CHENILLE_LF);
+		break;
+	case 2:
+		myScreen.Display(hw::S_CHENILLE_RS);
+		break;
+	case 3:
+		myScreen.Display(hw::S_CHENILLE_RF);
+		break;
+	case 4:
+		myScreen.Display(hw::S_CHENILLE_LS);
+		break;
+	case 5:
+		myScreen.Display(hw::S_BLUE);
+		break;
+	case 6:
+		myScreen.Display(hw::S_YELLOW);
+		break;
+	case 7:
+		myScreen.Display(hw::S_RED);
+		break;
+	case 8:
+		myScreen.Display(hw::S_BLUE_BS);
+		break;
+	case 9:
+		myScreen.Display(hw::S_BLUE_BF);
+		break;
+	case 10:
+		myScreen.Display(hw::S_BUZZ);
+		break;
+	default:
+		nbcycle1=0;
+		break;
+	}
+	if (nbcycle % 500==0) nbcycle1 = nbcycle1+1;
+		// execute myScreen to change screen state
+		myScreen.update();}
 
 
 } /* namespace test */
