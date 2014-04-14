f840629e293225ab75b700d1dedfd163e0c566b2
thomas.pareaud@free.fr <thomas.pareaud@free.fr>
2014-04-14 21:31:15 +0200

    Remove test comment.

--- autopilot/Autopilot/main.cpp
+++ autopilot/Autopilot/main.cpp
@@ -14,7 +14,6 @@
 #include <system/system/include/System.hpp>
 
 #include <gcs/include/SerialChannel.hpp>
-// test merge vincent
 board::Apm25::Param param = {
 	{ /* imu */
 		hw::HalImuMpu6000::E_GYR_CNF_500DPS, /* gyrCnf */
