383729c2a9ea287f0712689363c8d36afdf14790
aberzen <thomas.pareaud@free.fr>
2015-10-14 12:57:43 +0200

    Use channel enum instead of define. Reduce parameter scaling from 2^16 to 2^10.
    TODO: should be set in a define.

--- autopilot/Autopilot/nav/mgt/NavigationDirectThrust.cpp
+++ autopilot/Autopilot/nav/mgt/NavigationDirectThrust.cpp
@@ -25,11 +25,11 @@ NavigationDirectThrust::~NavigationDirectThrust()
 void NavigationDirectThrust::calcGuidance(
 		hw::Radio& radio)
 {
-	uint32_t thrustCmd = (int32_t) radio.getUnsigned(RADIO_IDX_THRUST);
+	int32_t thrustCmd = (int32_t) radio.getUnsigned(hw::Radio::E_RADIO_CHANNEL_THRUST);
 	system::system.dataPool.ctrlFrcDemB(
-			(_param.unitThrust[NAVIGATION_GUIDANCE_IDX_X] * thrustCmd) >> 16,
-			(_param.unitThrust[NAVIGATION_GUIDANCE_IDX_Y] * thrustCmd) >> 16,
-			(_param.unitThrust[NAVIGATION_GUIDANCE_IDX_Z] * thrustCmd) >> 16);
+			(_param.unitThrust[NAVIGATION_GUIDANCE_IDX_X] * thrustCmd) >> 10,
+			(_param.unitThrust[NAVIGATION_GUIDANCE_IDX_Y] * thrustCmd) >> 10,
+			(_param.unitThrust[NAVIGATION_GUIDANCE_IDX_Z] * thrustCmd) >> 10);
 }
 
 
